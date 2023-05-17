b04398a3730280eb8110a269037c36df039dfe3f
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2023-05-17 20:12:19 -0300

    owner field

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -22,6 +27,12 @@ class Contact(models.Model):
         Category, on_delete=models.SET_NULL,
         blank=True, null=True
         )
+    
+    owner = models.ForeignKey(
+       User,
+       on_delete=models.SET_NULL,
+       blank=True, null=True 
+    )
 
 
     def __str__(self) -> str:
