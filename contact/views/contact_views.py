6156e9c7c6fce6252e66339a8a642e9a1cb6422d
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2023-05-23 10:41:54 -0300

    return text

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -19,6 +19,34 @@ def index(request):
         context
     )
 
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value =='':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains = search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains = search_value) |
+            Q(email__icontains = search_value) 
+            
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
 
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
