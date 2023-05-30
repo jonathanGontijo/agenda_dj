cd85a325b8955272e58e28a1b975f037d496b1e4
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2023-05-30 15:27:40 -0300

    save contact

---
+++ contact/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name','last_name', 'phone',
+            'email', 'description', 'category',
+        )
+
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg =  ValidationError(
+                'Primeiro nome nao pode ser igual o segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        
+        
+        return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            raise ValidationError (
+                'Não digite ABC neste campo',
+                code='invalid'
+            )
+        return first_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,38 +1,20 @@
-from typing import Any, Dict
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render, redirect
-from django.db.models import Q
-from contact.models import Contact
-from django import forms
-from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
+from contact.forms import ContactForm
 
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name','last_name', 'phone',
-        )
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code = 'invalid'
-            )
-        )
-        
-        return super().clean()
 
 def create(request):
 
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         context = {
-        'form': ContactForm(request.POST)
+        'form': form
         }
+
+        if form.is_valid():
+            form.save()
+            return redirect('contact:create')
 
         return render(
             request,
